bf9e66cee5cbba71674f66987b5e5d3440cc1dfc
irrynka <irinakorcinska7@gmail.com>
2026-05-10 01:33:59 +0200

    Last Update

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,18 @@ from . import views #імпорт вюшки
 
 urlpatterns = [
     path('', views.home), #головний маршрут
+    path('history/', views.history, name='info'),
+    path('underground/', views.underground, name='underground'),
+    path('mision/', views.mision, name='mision'),
+    path('planttoom/', views.planttoom, name='planttoom'),
+    path('scafandersroom/', views.scafandersroom, name='scafandersroom'),
+    path('wateroom/', views.wateroom, name='wateroom'),
+    path('storage/', views.storage, name='storage'),
+    path('cafeteria/', views.cafeteria, name='cafeteria'),
+    path('dormitorium/', views.dormitorium, name='dormitorium'),
+    path('infirmary/', views.infirmary, name='infirmary'),
+    path('therapy/', views.therapy, name='therapy'),
+    path('breakroom/', views.breakroom, name='breakroom'),
+    path('soc/', views.soc, name='soc'),
+    path('VR/', views.VR, name='VR'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,47 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, "home.html") #Показ користовачу сторінку
+
+def history(request):
+    return render(request, "history.html") 
+
+def underground(request):
+    return render(request, "underground.html") 
+
+def mision(request):
+    return render(request, "mision.html") 
+
+def planttoom(request):
+    return render(request, "planttoom.html")
+
+def scafandersroom(request):
+    return render(request, "scafandersroom.html")
+
+def wateroom(request):
+    return render(request, "wateroom.html")
+
+def storage(request):
+    return render(request, "storage.html")
+
+def cafeteria(request):
+    return render(request, "cafeteria.html")
+
+def dormitorium(request):
+    return render(request, "dormitorium.html")
+
+def infirmary(request):
+    return render(request, "infirmary.html")
+
+def therapy(request):
+    return render(request, "therapy.html")
+
+def breakroom(request):
+    return render(request, "breakroom.html")
+
+def soc(request):
+    return render(request, "soc.html")
+
+def VR(request):
+    return render(request, "VR.html")
